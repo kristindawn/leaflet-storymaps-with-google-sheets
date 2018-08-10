77791bd167e79bb0c150cafce6ca027f4c401281
Kristin Lee <kristin.lee@tufts.edu>
2018-08-10 18:58:09 -0400

    Update storymap.js

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -234,14 +234,14 @@ $(window).on('load', function() {
       .prop("type", "text/css")
       .html("\
       #narration, #title {\
-        background-color: " + trySetting('_narrativeBackground', 'white') + "; \
+        background-color: " + trySetting('_narrativeBackground', '#958CD6') + "; \
         color: " + trySetting('_narrativeText', 'black') + "; \
       }\
       a, a:visited, a:hover {\
         color: " + trySetting('_narrativeLink', 'blue') + " \
       }\
       .in-focus {\
-        background-color: " + trySetting('_narrativeActive', '#A395C8') + " \
+        background-color: " + trySetting('_narrativeActive', '#D4D1EE') + " \
       }")
       .appendTo("head");
 
